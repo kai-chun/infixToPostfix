88a9bcdc1a8f0c6b8d456c365dd1fb519693da26
Kai-Chun Yang <32695855+kai-chun@users.noreply.github.com>
2018-01-02 20:02:54 +0800

    Update InfixToPostix.java

--- infixToPostfix/src/infixtopostix/InfixToPostix.java
+++ infixToPostfix/src/infixtopostix/InfixToPostix.java
@@ -16,17 +16,17 @@ import java.util.StringTokenizer;
 public class InfixToPostix { 
 
     static int priority(char c){  
-        if(c=='*'||c=='/'||c=='%'){  
+        if (c == '*' || c == '/' || c == '%'){  
             return 2;  
-        }else if(c=='+'||c=='-'){  
+        }else if (c == '+' || c == '-'){  
             return 1;  
         }else{  
             return 0;  
         }  
     }  
       
-    static boolean isOperator(char c){  
-        switch(c){  
+    static boolean isOperator (char c){  
+        switch (c){  
             case '(': case ')': case '+': case '-': case '*': case '/' : case '%':  
                 return true;  
             default:  
@@ -37,35 +37,35 @@ public class InfixToPostix {
     public static void main(String[] args) {  
         Scanner input = new Scanner(System.in);    
             
-        String s=input.next();    
-        Stack<String> st=new Stack<String>();  
-        StringTokenizer tokens = new StringTokenizer(s,"+|-|*|/|(|)|%",true);    
+        String s = input.next();    
+        Stack<String> st = new Stack<String>();  
+        StringTokenizer tokens = new StringTokenizer(s, "+|-|*|/|(|)|%", true);    
             
-        while(tokens.hasMoreTokens()){   
+        while (tokens.hasMoreTokens()){   
   
-            String c=tokens.nextToken();  
+            String c = tokens.nextToken();  
             String test;  
             boolean doit;  
               
-            if(isOperator(c.charAt(0))==false){  
-                System.out.print(c+" ");  
+            if (isOperator(c.charAt(0)) == false){  
+                System.out.print(c + " ");  
             }else{  
-                if(c.charAt(0) == '('){  
+                if (c.charAt(0) == '('){  
                     st.push(c);  
-                }else if(c.charAt(0) == ')'){  
-                    while(st.peek().charAt(0) != '('){  
-                        System.out.print(st.pop()+" ");  
+                }else if (c.charAt(0) == ')'){  
+                    while (st.peek().charAt(0) != '('){  
+                        System.out.print(st.pop() + " ");  
                     }  
                     st.pop();  
                 }else{  
-                    if(st.empty()){  
+                    if (st.empty()){  
                         st.push(c);               
                     }else{                        
-                        doit=true;  
-                        while(!st.empty() && doit){  
-                            test=st.pop();  
-                            if(priority(test.charAt(0))>=priority(c.charAt(0))){  
-                                System.out.print(test+" ");  
+                        doit = true;  
+                        while (!st.empty() && doit){  
+                            test = st.pop();  
+                            if (priority(test.charAt(0)) >= priority(c.charAt(0))){  
+                                System.out.print(test + " ");  
                             }else{  
                                 st.push(test);  
                                 doit = false;  
@@ -76,8 +76,8 @@ public class InfixToPostix {
                 }  
             }  
         }  
-        while(!st.empty()){  
-            System.out.print(st.pop()+" ");  
+        while (!st.empty()){  
+            System.out.print(st.pop() + " ");  
         }  
         System.out.println();  
     }   
