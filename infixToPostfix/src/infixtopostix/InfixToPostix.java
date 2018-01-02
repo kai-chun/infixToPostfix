b3d827ea02ccfe792faaf42adf4c7cae2e648774
Kai-Chun Yang <32695855+kai-chun@users.noreply.github.com>
2018-01-02 20:37:55 +0800

    Update InfixToPostix.java
    增加註解

--- infixToPostfix/src/infixtopostix/InfixToPostix.java
+++ infixToPostfix/src/infixtopostix/InfixToPostix.java
@@ -15,7 +15,7 @@ import java.util.StringTokenizer;
  */
 public class InfixToPostix { 
 
-    static int priority(char c){  
+    static int priority(char c){  // 運算的優先順序
         if (c == '*' || c == '/' || c == '%'){  
             return 2;  
         }else if (c == '+' || c == '-'){  
@@ -25,7 +25,7 @@ public class InfixToPostix {
         }  
     }  
       
-    static boolean isOperator (char c){  
+    static boolean isOperator (char c){  // 判斷是否為運算子
         switch (c){  
             case '(': case ')': case '+': case '-': case '*': case '/' : case '%':  
                 return true;  
@@ -39,7 +39,7 @@ public class InfixToPostix {
             
         String s = input.next();    
         Stack<String> st = new Stack<String>();  
-        StringTokenizer tokens = new StringTokenizer(s, "+|-|*|/|(|)|%", true);    
+        StringTokenizer tokens = new StringTokenizer(s, "+|-|*|/|(|)|%", true);  // 拆解運算式  
             
         while (tokens.hasMoreTokens()){   
   
@@ -47,26 +47,26 @@ public class InfixToPostix {
             String test;  
             boolean doit;  
               
-            if (isOperator(c.charAt(0)) == false){  
+            if (isOperator(c.charAt(0)) == false){ // 若不是運算子，直接印出運算元  
                 System.out.print(c + " ");  
             }else{  
-                if (c.charAt(0) == '('){  
+                if (c.charAt(0) == '('){  // 看到左括弧，直接 push 
                     st.push(c);  
-                }else if (c.charAt(0) == ')'){  
+                }else if (c.charAt(0) == ')'){  // pop 直到遇到左括弧
                     while (st.peek().charAt(0) != '('){  
                         System.out.print(st.pop() + " ");  
                     }  
                     st.pop();  
                 }else{  
-                    if (st.empty()){  
+                    if (st.empty()){  // 若 stack 為空，push next token
                         st.push(c);               
                     }else{                        
                         doit = true;  
                         while (!st.empty() && doit){  
                             test = st.pop();  
-                            if (priority(test.charAt(0)) >= priority(c.charAt(0))){  
+                            if (priority(test.charAt(0)) >= priority(c.charAt(0))){  // 若 stack 最上層的運算子優先序大於現在讀入的則印出
                                 System.out.print(test + " ");  
-                            }else{  
+                            }else{  // 不是的話，把 pop 出來的放回去
                                 st.push(test);  
                                 doit = false;  
                             }  
@@ -76,7 +76,7 @@ public class InfixToPostix {
                 }  
             }  
         }  
-        while (!st.empty()){  
+        while (!st.empty()){  // 把 stack 剩下的 token 全部印出來
             System.out.print(st.pop() + " ");  
         }  
         System.out.println();  
